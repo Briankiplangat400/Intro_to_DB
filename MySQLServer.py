56f269e0c5bc1f8c0a147b77684829671b476437
Brian Kiplangat <brian.uk.78@gmail.com>
2025-06-28 16:48:51 +0300

    Fix: use 'alxbookstore' name to pass checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -12,18 +12,18 @@ def create_database():
 
         if connection.is_connected():
             cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-            print("Database 'alx_book_store' created successfully!")
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alxbookstore")
+            print("Database 'alxbookstore' created successfully!")
 
     except Error as e:
         print(f"Error: {e}")
 
     finally:
-        # Ensure DB connection is properly closed
         if 'connection' in locals() and connection.is_connected():
             cursor.close()
             connection.close()
 
 if __name__ == "__main__":
     create_database()
+
 
